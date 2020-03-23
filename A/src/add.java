397959ff173eb702e44256729c36f29e4206d0ef
DELL <DELL@192.168.0.7>
2020-03-23 23:17:55 +0530

    first commit

---
+++ A/src/add.java
@@ -0,0 +1,14 @@
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   String ch="jayapal";
+   System.out.println(ch);
+  
+   for(int j=ch.length()-1;j>=0;j--){System.out.println(ch.charAt(j));
+   System.out.println("jayapal");
+   }
+	}
+
+}
